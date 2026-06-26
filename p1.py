3fe7ec483ef8c648c4d9aca09265e5ffd83e7969
Vinka Antonijevic <vavinkaaa27@gmail.com>
2026-06-26 17:05:44 +0000

    Fix flake8 errors

--- p1.py
+++ p1.py
@@ -2,5 +2,4 @@ import pandas as pd
 
 lst = ['Geeks', 'For', 'Geeks', 'is', 'portal', 'for', 'Geeks']
 df = pd.DataFrame(lst)
-
 print(df)
--- p2.py
+++ p2.py
@@ -1,12 +1,11 @@
-import    numpy as np
-import pandas as   pd
+import numpy as np
+import pandas as pd
 
-data = { 
-                        'A': np.array([1, 4, 7]),
-         'B': np.array([2,    5,8
-         ]),
-         'C': 
-         np.array(    [3, 6,     9])     }
-df = pd.DataFrame(      data)
-print(
- df)
+data = {
+    'A': np.array([1, 4, 7]),
+    'B': np.array([2, 5, 8]),
+    'C': np.array([3, 6, 9]),
+}
+
+df = pd.DataFrame(data)
+print(df)
